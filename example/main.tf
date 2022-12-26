591aab49a19f291da84d80825af862c6562ab026
Manish Bisht <manishbisht@Manishs-MacBook-Pro.local>
2022-12-26 18:18:42 +0530

    [added example]

--- example/main.tf
+++ example/main.tf
@@ -6,5 +6,5 @@ module "dynamodb_archive" {
   lambda_iam_role_name = "lambda_role"
   firehose_iam_role_name = "firehose_role"
   lambda_function_name = "write_to_kinesis"
-  dynamodb_table_stream_arn = "arn:aws:dynamodb:us-west-1:1234567890:table/dynamodb_table/stream/2022-12-15T00:00:11.804""
+  dynamodb_table_stream_arn = "arn:aws:dynamodb:us-west-1:1234567890:table/dynamodb_table/stream/2022-12-15T00:00:11.804"
 }
