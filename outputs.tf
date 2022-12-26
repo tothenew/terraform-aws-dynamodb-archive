e312171389df2bdd6638f742a44d303ffd8e9b62
Manish Bisht <manishbisht@Manishs-MacBook-Pro.local>
2022-12-26 19:49:44 +0530

    [separated iam]

--- outputs.tf
+++ outputs.tf
@@ -3,17 +3,9 @@ output "kinesis-firehose-stream-arn" {
 }
 
 output "lambda_iam_role_arn" {
-  value = module.iam_resources.lambda_aws_role_arn
+  value = module.lambda_iam.lambda_aws_role_arn
 }
 
 output "firehose_iam_role_arn" {
-  value = module.iam_resources.firehose_iam_role_arn
-}
-
-output "lambda_iam_policy_arn" {
-  value = module.iam_resources.lambda_iam_policy_arn
-}
-
-output "firehose_iam_policy_arn" {
-  value = module.iam_resources.firehose_iam_policy_arn
+  value = module.firehose_iam.firehose_iam_role_arn
 }
